58612b498916161284046d15dce4b69bad5aaaa0
blog-lyn <a@s76.org>
2017-06-03 23:19:45 +0800

    fix(engine.js): 修改console内容

--- engine.js
+++ engine.js
@@ -123,7 +123,7 @@ module.exports = function (options) {
         var footer = filter([ breaking, issues ]).join('\n\n');
 
         commit(head + '\n\n' + body + '\n\n' + footer);
-        console.log('运行CI后,可以生成changelog和修改package版本号,最后push代码')
+        console.log('\n 运行CI后,再生成changelog和修改package版本号,最后push代码 \n')
       });
     }
   };
